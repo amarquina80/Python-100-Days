926192cabdd838b423be6f806bfa2ac279fe00aa
Alexander Marquina <114547786+amarquina80@users.noreply.github.com>
2022-11-27 15:16:04 -0800

    day 1

---
+++ day1.py
@@ -0,0 +1,7 @@
+print ("Alexander")
+print ("November 27th 2022")
+print ("I am signing up for Replit's 100 days of Python challenge!")
+print ("I will make sure to spend time every day coding along, for the minimum of 10 minutes a day.")
+print ("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!")
+print ("I am feeling 🤩")
+print ("You can follow my progress at replit.com/@amarquina80")
