ccb0b50dd45ca47589f35ab565b6f6341a444e76
Alexander Marquina <114547786+amarquina80@users.noreply.github.com>
2022-11-29 22:37:53 -0800

    day3

---
+++ day3.py
@@ -0,0 +1,16 @@
+myName = input("What's your name?")
+myLunch =input("what are you having for lunch?")
+print(myName, "is going to be chowing on", myLunch, "very soon")
+
+yourName = input ("Name: ")
+whatYear = input ("what year is it?: ")
+print(yourName, "thinks it is", whatYear)
+
+foodName = input("Name a food >")
+plantName = input("Name a type of plant >")
+cookingMethod =input ("Name a method of cooking >")
+burnedFood = input("What word describes burned food? >")
+diyItem= input("Name a DIY item >")
+
+print("Menu")
+print(cookingMethod, foodName, "with", plantName, "on the bed of", diyItem)
