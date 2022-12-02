890d5cc97c3498065daff0c896b2cae13d5211c4
Alexander Marquina <amarquina@Alexanders-MacBook-Air.local>
2022-12-02 10:28:42 -0800

    day6

---
+++ day6.py
@@ -0,0 +1,37 @@
+print("SECURE LOGIN")
+username = input("Username: ")
+password = input ("Password: ")
+
+if username == "Alex" and password == "letmein":
+  print("oh hey Alex!")
+elif username == "Gonzalo" and password == "bueno?":
+  print("hola chuyuyuy!")
+else:
+  print("Who are you again?")
+
+season = input("what is your favorite season?")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+      print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
+
+print("MY SECURE LOGIN SYSTEM")
+print("++++++++++++++++++++++")
+
+username = input ("Username:")
+password = input ("Password:")
+
+if username == "alex" and password == "letmein!":
+  print("Magandang umaga Alex! You are doing good, keep going and you will reach your dream!")
+
+elif username =="gonzalo" and password == "bueno?":
+  print("Buenos dias Chuyuyuy, ya estamos haciendo bien, siguete practicar mas para conseguir tus seunos")
+
+else:
+  print("you must be in the wrong room,pls try again")
