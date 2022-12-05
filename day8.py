ad0b79eadee1777040e72738d346fff8f2efa652
Alexander Marquina <amarquina@Alexanders-MacBook-Air.local>
2022-12-05 00:19:01 -0800

    day8

---
+++ day8.py
@@ -0,0 +1,33 @@
+
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name?")
+
+
+
+if name =="Alex" or name == "alex":
+ DOW = input("What is the day of the week?")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be a lovely Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("What a peaceful Tuesday it is", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("Happy Fun Day", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"your almost there!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "It's FRIYAAAAY!")
+
+elif name == "Gonzalo" or name== "gonzalo":
+ DOW = input("What is the day of the week?")
+ if DOW == "monday" or DOW == "Monday":
+   print("It is going to be anawesome Monday", name)
+ if DOW == "tuesday" or DOW == "Tuesday":
+   print("You look great", name)
+ if DOW == "wednesday" or DOW == "Wednesday":
+   print("looking good", name)
+ if DOW == "thursday" or DOW == "Thursday":
+   print(name,"great job!")
+ if DOW == "friday" or DOW == "Friday":
+   print(name, "it's Funday!")
+else:
+ print("Who are you, let's start the day positively!")
